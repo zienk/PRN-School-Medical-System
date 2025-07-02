45f7c8517af7d0b85634a4b99ad70ce992bab019
zienk <viennnse182672@fpt.edu.vn>
2025-07-02 16:14:16 +0700

    Develop: user repository and service implementations for login function

---
+++ Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly PrnEduHealthContext _context;
+
+        public UserRepository()
+        {
+            _context = new PrnEduHealthContext();
+        }
+
+        public User? GetUser(string username, string password)
+        {
+            return _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefault(u => u.Username == username && u.Password == password && u.IsActive == true);
+        }
+
+
+    }
+}
