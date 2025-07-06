325979f408140b5483bf5490099af19dbb315a0f
zienk <viennnse182672@fpt.edu.vn>
2025-07-06 19:26:06 +0700

    Add: dashboards UI for all

--- WPF.SchoolMedicalManagementSystem/ManagerView/ManagerDashboard.xaml.cs
+++ WPF.SchoolMedicalManagementSystem/ManagerView/ManagerDashboard.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPF.SchoolMedicalManagementSystem.ManagerView
@@ -18,7 +17,7 @@ namespace WPF.SchoolMedicalManagementSystem.ManagerView
     /// <summary>
     /// Interaction logic for ManagerDashboard.xaml
     /// </summary>
-    public partial class ManagerDashboard : Page
+    public partial class ManagerDashboard : Window
     {
         public ManagerDashboard()
         {
