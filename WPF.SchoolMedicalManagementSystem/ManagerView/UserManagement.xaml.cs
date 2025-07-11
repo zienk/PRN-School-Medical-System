b70c70750d269a866a91cccee5cb0da4cbae1895
zienk <viennnse182672@fpt.edu.vn>
2025-07-11 19:42:31 +0700

    Fix: Call UpdateRecordCount in UserManagement constructor

--- WPF.SchoolMedicalManagementSystem/ManagerView/UserManagement.xaml.cs
+++ WPF.SchoolMedicalManagementSystem/ManagerView/UserManagement.xaml.cs
@@ -30,6 +30,7 @@ namespace WPF.SchoolMedicalManagementSystem.ManagerView
             InitializeComponent();
             _userService = new UserService();
             LoadUserData();
+            UpdateRecordCount();
         }
 
         private void LoadUserData()
