a20d755aeb23565fcafb42e77afbe63be87b0bd4
Thien-isme <thienisme2004@gmail.com>
2025-07-11 17:42:55 +0700

    Add: parent view HealthCheckupResult

---
+++ Services/Interfaces/IHealthCheckupResultService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects.Entities;
+namespace Services.Interfaces
+{
+    public interface IHealthCheckupResultService
+    {
+        //Thien
+        public List<HealthCheckupResult> getAllHealthCheckupResultByStudentId(int studentId);
+    }
+}
