40dbb6768d40296855e5a3004a35f8fd06b0f8f8
zienk <viennnse182672@fpt.edu.vn>
2025-07-11 19:26:57 +0700

    Add: CRUD and search for HealthCheckup entity

--- Services/Implementations/HealthCheckupService.cs
+++ Services/Implementations/HealthCheckupService.cs
@@ -23,5 +23,49 @@ namespace Services.Implementations
         {
             return _healthCheckupRepository.AddHealthCheckup(healthCheckup);
         }
+
+        public bool DeleteHealthCheckup(int healthCheckupId)
+        {
+            var existingCheckup = _healthCheckupRepository.GetHealthCheckupById(healthCheckupId);
+            
+            if (existingCheckup == null)
+                return false;
+
+            return _healthCheckupRepository.DeleteHealthCheckup(healthCheckupId);
+        }
+
+        public List<HealthCheckup> GetAllHealthCheckups()
+        => _healthCheckupRepository.GetAllHealthCheckups();
+
+        public HealthCheckup? GetHealthCheckupById(int healthCheckupId)
+        {
+            if (healthCheckupId <= 0)
+                return null;
+
+            return _healthCheckupRepository.GetHealthCheckupById(healthCheckupId);
+        }
+
+        public List<HealthCheckup> GetHealthCheckupsByCreatorId(Guid creatorId)
+        => _healthCheckupRepository.GetHealthCheckupsByCreatorId(creatorId);
+
+        public List<HealthCheckup> SearchHealthCheckups(string searchText)
+        => _healthCheckupRepository.SearchHealthCheckups(searchText ?? "");
+
+        public bool UpdateHealthCheckup(HealthCheckup healthCheckup)
+        {
+            if (healthCheckup == null || healthCheckup.CheckupId <= 0)
+                return false;
+            
+            var existingCheckup = _healthCheckupRepository.GetHealthCheckupById(healthCheckup.CheckupId);
+
+            if (existingCheckup == null)
+                return false;
+
+            existingCheckup.CheckupName = healthCheckup.CheckupName;
+            existingCheckup.CheckupDate = healthCheckup.CheckupDate;
+            existingCheckup.Description = healthCheckup.Description;
+
+            return _healthCheckupRepository.UpdateHealthCheckup(existingCheckup);
+        }
     }
 }
